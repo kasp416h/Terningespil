786ebc95da3c7eab4e0d8f6f1d101cb594043afd
kasp416h <HFGF@DESKTOP-M7OHMSR>
2023-08-09 13:15:25 +0200

    Add project files.

---
+++ Terningespil/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Terningespil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
