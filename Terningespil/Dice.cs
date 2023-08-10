5aeb34398cd720a7460db806c8ea52d9c5b24e2d
kasp416h <HFGF@DESKTOP-M7OHMSR>
2023-08-10 10:59:12 +0200

    alt

---
+++ Terningespil/Dice.cs
@@ -0,0 +1,16 @@
+﻿namespace Terningespil
+{
+    public class Dice
+    {
+        private int sides;
+        private Random rnd = new Random();
+        public Dice(int numberOfSides)
+        {
+            sides = numberOfSides;
+        }
+        public int Roll()
+        {
+            return rnd.Next(1, sides + 1);
+        }
+    }
+}
--- Terningespil/Program.cs
+++ Terningespil/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace Terningespil
+﻿using static Terningespil.Dice;
+namespace Terningespil
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hvor mange terninger vil du slå med");
+            int diceCount = Int32.Parse(Console.ReadLine());
+
+            int trys = 0;
+
+            while (true)
+            {
+                List<int> rolls = new List<int>();
+
+                for (int index = 0; index < diceCount; index++)
+                {
+                    var dice = new Dice(6);
+                    rolls.Add(dice.Roll());
+                }
+
+                bool isAllSix = true;
+
+                foreach (int roll in rolls)
+                {
+                    Console.Write(roll + " ");
+                    if (roll != 6) 
+                    { 
+                        isAllSix = false;
+                    }
+                }
+
+                Console.WriteLine();
+                trys++;
+
+                if (isAllSix)
+                {
+                    Console.WriteLine(trys);
+                    break;
+                } else
+                {
+                    continue;
+                }
+            }
+            
         }
     }
 }
